dd9379742311e3523944bc062f9a52b6ac64cc62
Mateo <141548968+elemeTr0@users.noreply.github.com>
2025-03-17 22:24:24 +0100

    Added Register Window

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 document.getElementById('login').addEventListener('click', function() {
-    var container = document.querySelector('.logwin');
-    if (container.style.display === 'block') {
-        container.style.display = 'none';
-    } else {
-        container.style.display = 'block';
-    }
+    var logContainer = document.querySelector('.logwin');
+    var regContainer = document.querySelector('.regwin');
+
+    logContainer.style.display = logContainer.style.display === 'block' ? 'none' : 'block';
+    regContainer.style.display = 'none';
 });
 
-console.clear();
-Splitting();
+document.getElementById('reg').addEventListener('click', function() {
+    var regContainer = document.querySelector('.regwin');
+    var logContainer = document.querySelector('.logwin');
+
+    regContainer.style.display = regContainer.style.display === 'block' ? 'none' : 'block';
+    logContainer.style.display = 'none';
+});
+
